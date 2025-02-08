0815361a0a0910565242df6bf94dade4f59ae7c2
TBau23 <tom.a.bauer@protonmail.com>
2025-02-07 21:26:10 -0600

    reset

---
+++ demo.py
@@ -0,0 +1,3 @@
+
+arr = [1, 2, 3, 4, 5]
+print(arr[8] if arr[8] is not None else None)
--- src/main.py
+++
@@ -1,7 +0,0 @@
-from textnode import TextNode, TextType
-
-def main():
-    res = TextNode('First node', TextType.BOLD, 'https://www.google.com')
-    print(res)
-
-main()
